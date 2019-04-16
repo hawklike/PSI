4f6df009e58a4d2990e2ff15e530e2d909a5ab1f
Jan Steuer <jan.steuer@centrum.cz>
2019-04-16 18:45:43 +0200

    Updated main

--- src/cz/fit/cvut/steuejan/psi/serverClient/Main.java
+++ src/cz/fit/cvut/steuejan/psi/serverClient/Main.java
@@ -2,10 +2,13 @@ package cz.fit.cvut.steuejan.psi.serverClient;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
-	// write your code here
+	    int port = Integer.parseInt(args[0]);
+        int serverAddress = Integer.parseInt(args[1]);
+
+        Server server = new Server();
+
     }
 
 
---
+++ src/cz/fit/cvut/steuejan/psi/serverClient/Server.java
@@ -0,0 +1,5 @@
+package cz.fit.cvut.steuejan.psi.serverClient;
+
+public class Server
+{
+}
