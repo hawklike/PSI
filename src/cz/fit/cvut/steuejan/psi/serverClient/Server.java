74b71ee61d93093f2a01bd204e618e88e60aa62a
Jan Steuer <jan.steuer@centrum.cz>
2019-04-29 16:39:58 +0200

    The program successfully ends after all tests are passed

--- src/cz/fit/cvut/steuejan/psi/serverClient/Server.java
+++ src/cz/fit/cvut/steuejan/psi/serverClient/Server.java
@@ -7,8 +7,8 @@ class Server
 {
     void start(int port)
     {
-        try{serverSocket = new ServerSocket(port);}
-        catch(IOException exc) {System.out.println("Unable to open the socket: " + exc);}
+        try{ serverSocket = new ServerSocket(port); }
+        catch(IOException exc) { System.out.println("Unable to open the socket: " + exc); }
         System.out.println("Server successfully started.");
     }
 
@@ -16,11 +16,11 @@ class Server
     {
         try
         {
-            while(true)
+            while(nTests++ < NUMBER_OF_TESTS)
             {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println();
-                System.out.println("Running test " + ++nTests);
+                System.out.println("Running test " + nTests);
                 System.out.println("New client connected.");
                 new Thread(new ServerThread(clientSocket)).start();
             }
@@ -30,5 +30,6 @@ class Server
 
     private ServerSocket serverSocket;
     private int nTests = 0;
+    private static final int NUMBER_OF_TESTS = 32;
 
 }
